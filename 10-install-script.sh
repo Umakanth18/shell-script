3f5489269925fc82fed9a3ccfb4a49a69d24056e
Umakanth <umakanth.munna1234@gmail.com>
2025-02-07 00:20:39 -0500

    shell-script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -10,4 +10,20 @@ fi
 
 dnf install mysql -y
 
+if [ $? -ne 0 ]
+then
+    echo "Installing Git is....Failure"
+    exit 1
+else
+    echo "Installing GIT IS....Success"
+fi 
+
 dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installing GIT is....Failure"
+    exit 1
+else
+    echo "Installing GIT is.....Success"
+fi        
